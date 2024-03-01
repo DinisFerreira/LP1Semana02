de6e8b1e1563c90c63c87c4f8b62c3e8799af77d
Dinis <dinisvferreira@gmail.com>
2024-03-01 15:02:53 +0000

    add número inteiro não-negativo

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("Insert a non negative integer: ");
+            string input = Console.ReadLine();
+
+            // Conversion
+            byte n = byte.Parse(input);
+
+            //increase and decrease
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+
+            //Division, shift, xor, >10
+            Console.WriteLine(n/2);
+            Console.WriteLine(n<<3);
+            Console.WriteLine(n^5);
+            Console.WriteLine(n>10);
+
+           
+            
+    }
+}
